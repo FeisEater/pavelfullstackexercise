97daab46505faa82bf3c8264ba14df1c414fb6d5
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-01-20 19:18:56 +0200

    tehtävä 4

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,28 +3,30 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    }
-    const osa2 = {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    }
-    const osa3 = {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
+    const osat = [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
   
     return (
-      <div>
-        <Otsikko kurssi={kurssi} />
-        <Sisalto osa1={osa1} osa2={osa2} osa3={osa3}/>
-        <Yhteensa yhteensa={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} />
-      </div>
+        <div>
+            <Otsikko kurssi={kurssi} />
+            <Sisalto osat={osat} />
+            <Yhteensa osat={osat} />
+        </div>
     )
-  }
-  
+}
+    
 const Otsikko = (props) => {
     return (
         <h1>{props.kurssi}</h1>
@@ -34,9 +36,9 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa osa={props.osa1} />
-            <Osa osa={props.osa2} />
-            <Osa osa={props.osa3} />
+            <Osa osa={props.osat[0]} />
+            <Osa osa={props.osat[1]} />
+            <Osa osa={props.osat[2]} />
         </div>
       )
 }
@@ -49,7 +51,7 @@ const Osa = (props) => {
 
 const Yhteensa = (props) => {
     return (
-        <p>yhteensä {props.yhteensa} tehtävää</p>
+        <p>yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää</p>
     )
 }
 
