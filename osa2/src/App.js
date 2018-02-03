13da8706c7f78ba18db74403d9e2629854b23bdb
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-02-04 01:46:39 +0200

    Tehtävä 2.6

--- osa2/src/App.js
+++ osa2/src/App.js
@@ -1,50 +1,60 @@
 import React from 'react'
-import Kurssi from './components/kurssi'
 
-const App = () => {
-    const kurssit = [
-      {
-        nimi: 'Half Stack -sovelluskehitys',
-        id: 1,
-        osat: [
-          {
-            nimi: 'Reactin perusteet',
-            tehtavia: 10,
-            id: 1
-          },
-          {
-            nimi: 'Tiedonvälitys propseilla',
-            tehtavia: 7,
-            id: 2
-          },
-          {
-            nimi: 'Komponenttien tila',
-            tehtavia: 14,
-            id: 3
-          }
-        ]
-      },
-      {
-        nimi: 'Node.js',
-        id: 2,
-        osat: [
-          {
-            nimi: 'Routing',
-            tehtavia: 3,
-            id: 1
-          },
-          {
-            nimi: 'Middlewaret',
-            tehtavia: 7,
-            id: 2
-          }
-        ]
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        persons: [
+          { name: 'Arto Hellas', id: 0 }
+        ],
+        newName: ''
       }
-    ]
+    }
+  
+    addEntry = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: this.state.newName,
+            id: this.state.persons.length
+        }
+        
+        const persons = this.state.persons.concat(personObject)
+        
+        this.setState({
+            persons,
+            newName: ''
+        })
+    }
 
+    handleChange = (event) => {
+        this.setState({ newName: event.target.value })
+    }
+      
+    render() {
+      return (
+        <div>
+          <h2>Puhelinluettelo</h2>
+          <form onSubmit={this.addEntry}>
+            <div>
+              nimi: <input value={this.state.newName} onChange={this.handleChange} />
+            </div>
+            <div>
+              <button type="submit">lisää</button>
+            </div>
+          </form>
+          <h2>Numerot</h2>
+          <div>
+              {this.state.persons.map(person => <Person key={person.id} person={person} />)}
+          </div>
+        </div>
+      )
+    }
+  }
+
+const Person = (props) => {
     return (
         <div>
-            {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
+            {props.person.name}
         </div>
     )
 }
