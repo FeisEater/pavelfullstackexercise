a4461aa0c4c4ad20883e9f9443fe9593e66057eb
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-02-18 02:43:44 +0200

    tehtävä 4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -9,7 +9,22 @@ const totalLikes = (blogs) => {
     return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0)
+        return {}
+    let sortedBlogs = blogs.slice()
+    sortedBlogs.sort((a, b) => {
+        return b.likes - a.likes
+    })
+    return {
+        title: sortedBlogs[0].title,
+        author: sortedBlogs[0].author,
+        likes: sortedBlogs[0].likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
