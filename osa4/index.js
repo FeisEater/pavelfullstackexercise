fe96cc2475694d5bc93f2c92d5e8b376cea23c02
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-02-20 04:03:12 +0200

    tehtävä 4.8

--- osa4/index.js
+++ osa4/index.js
@@ -4,18 +4,34 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
+const config = require('./utils/config')
 const blogRouter = require('./controllers/blogcontroller')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogRouter)
 
-const mongoUrl = 'salaisuus'
-mongoose.connect(mongoUrl)
+mongoose
+.connect(config.mongoUrl)
+.then(() => {
+    console.log('connected to database', config.mongoUrl)
+})
+.catch(err => {
+    console.log(err)
+})
 mongoose.Promise = global.Promise
 
-const PORT = 3003
-app.listen(PORT, () => {
+const server = http.createServer(app)
+
+const PORT = config.port
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+    app, server
+}
