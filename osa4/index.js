b4de0264679d019d1e42a3d1fcf7f8d54759e88d
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-02-25 15:54:08 +0200

    tehtävä 4.15

--- osa4/index.js
+++ osa4/index.js
@@ -6,10 +6,12 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blogcontroller')
+const userRouter = require('./controllers/usercontroller')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 mongoose
 .connect(config.mongoUrl)
