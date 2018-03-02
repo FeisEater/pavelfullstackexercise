80092727f4fc4415ccc3cc2735916d0eeef2b234
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-03-02 20:21:32 +0200

    tehtävä 5.13

--- osa5/src/components/SimpleBlog.test.js
+++ osa5/src/components/SimpleBlog.test.js
@@ -17,4 +17,22 @@ describe.only('<SimpleBlog />', () => {
     expect(contentDiv.text()).toContain(`${blog.title} ${blog.author}`)
     expect(likesDiv.text()).toContain(` ${blog.likes} `)
   })
+
+  it('clicking the like button twice calls event handler twice', () => {
+    const blog = {
+        title: 'testTitle',
+        author: 'testAuthor',
+        likes: 5
+    }
+  
+    const mockHandler = jest.fn()
+
+    const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+  
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
