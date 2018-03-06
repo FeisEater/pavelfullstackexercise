22bc0aa729ff2381a1d7d7ee9c295cbe8df61af5
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-03-06 15:17:45 +0200

    tehtävä 6.22

--- osa6/src/App.js
+++ osa6/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
-import { Container, Table } from 'semantic-ui-react'
+import { Container, Table, Grid, Image } from 'semantic-ui-react'
 
 const Menu = () => {
   const containerStyle = {
@@ -38,17 +38,22 @@ const AnecdoteList = ({ anecdotes }) => (
 )
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
+  <Grid>
+    <Grid.Column width={12}>
+      <h2>About anecdote app</h2>
+      <p>According to Wikipedia:</p>
+      
+      <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+        Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+        such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+        An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
+      <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <Image src='/carmack.jpg' />
+    </Grid.Column>
+  </Grid>
 )
 
 const Footer = () => (
