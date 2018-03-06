872addd0b0d90bfff789881fba1b17b0e1849fd6
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-03-06 11:56:01 +0200

    refinements

--- osa6/src/components/AnecdoteList.js
+++ osa6/src/components/AnecdoteList.js
@@ -3,36 +3,36 @@ import { vote } from '../reducers/anecdoteReducer'
 import { showInfo, hide } from '../reducers/notificationReducer'
 import { connect } from 'react-redux'
 
-class AnecdoteList extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Anecdotes</h2>
-        {this.props.anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => {
-                this.props.vote(anecdote.id)
-                this.props.showInfo('voted for "' + anecdote.content + '"')
-                setTimeout(() => this.props.hide(), 5000)
-              }}>
-                vote
-              </button>
-            </div>
+const AnecdoteList = (props) => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {props.anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
           </div>
-        )}
-      </div>
-    )
-  }
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => doVote(anecdote, props)}>
+              vote
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+const doVote = (anecdote, props) => {
+  props.vote(anecdote.id)
+  props.showInfo('voted for "' + anecdote.content + '"')
+  setTimeout(() => props.hide(), 5000)
 }
 
 const mapStateToProps = (state) => {
   return {
-    anecdotes: state.anecdotes.filter(a => a.content.indexOf(state.filter) !== -1),
+    anecdotes: state.anecdotes.filter(a => a.content.indexOf(state.filter) !== -1).sort((a, b) => b.votes - a.votes),
     filter: state.filter
   }
 }
